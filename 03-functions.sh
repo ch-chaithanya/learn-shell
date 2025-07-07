7786afe2f22bb4493dca7ce0bf27f52624b3e4c6
chaithanya <chaithanyack656@gmail.com>
2025-07-07 21:26:50 +0530

    roboshop code

--- 03-functions.sh
+++ 03-functions.sh
@@ -17,3 +17,23 @@ SAMPLE
 
 source common.sh
 demo
+
+
+# Variables declared in out side of functions can be accessible inside function and vice-versa.
+
+
+#       Before execution
+# LHS   function  RHS
+
+
+abc1() {
+  echo value of a - $a
+  echo value of b - $b
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments - $*
+  echo Arguments Count - $#
+}
+
+a=10
+b=20 abc1 100 200
